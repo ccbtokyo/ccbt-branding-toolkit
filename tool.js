6e30465c6be29413ac1671189272171ae477b84f
hysysk <hayashi.yosuke@gmail.com>
2022-06-19 19:23:34 +0900

    Add Import/Export

--- tool.js
+++ tool.js
@@ -1,4 +1,5 @@
 import g from "./global.js";
+import Shape from "./shape.js";
 
 class Tool {
   constructor() {
@@ -57,6 +58,40 @@ class Tool {
         const shapeIndex = Math.floor(Math.random() * shapeTypes.length);
         g.shapes[i].type = shapeTypes[shapeIndex];
       }
+    });
+
+    const exportShape = document.getElementById('export');
+    exportShape.addEventListener('click', () => {
+      const txt = JSON.stringify(g.shapes);
+      const file = new File([txt], 'logo.json', { type: 'text/plain' });
+      const url = URL.createObjectURL(file);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = file.name;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    });
+
+    const importShape = document.getElementById('import');
+    importShape.addEventListener('change', e => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.addEventListener('load', e => {
+        const shapes = JSON.parse(e.target.result);
+        for(let i=0; i<shapes.length; i++) {
+          let s = new Shape(shapes[i].x, shapes[i].y, shapes[i].shape, shapes[i].color);
+          g.shapes.push(s);
+        }
+      });
+      reader.readAsText(file);
+    });
+
+    const clear = document.getElementById('clear');
+    clear.addEventListener('click', () => {
+      g.shapes = [];
+      g.ctx.clearRect(0, 0, g.CANVAS_SIZE, g.CANVAS_SIZE);
     });
   }
 }
