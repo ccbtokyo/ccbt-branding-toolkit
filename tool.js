75425c0efcb078f668d78e5f9d482db45d1ce2d8
hysysk <hayashi.yosuke@gmail.com>
2022-07-24 11:44:23 +0900

    Remove unnecessary functions and brush up the UI

--- tool.js
+++ tool.js
@@ -3,69 +3,36 @@ import Shape from "./shape.js";
 
 class Tool {
   constructor() {
-    const buttons = document.querySelectorAll('#tool .mode');
+    const buttons = document.querySelectorAll("#tool .mode");
     for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', () => {
+      buttons[i].addEventListener("click", () => {
         g.currentMode = buttons[i].value;
+        if (!buttons[i].classList.contains("selected")) {
+          for (let j = 0; j < buttons.length; j++) {
+            buttons[j].classList.remove("selected");
+          }
+          buttons[i].classList.add("selected");
+        }
       });
     }
 
-    const color = document.getElementById('color');
-    color.addEventListener('input', () => {
+    const color = document.getElementById("color");
+    color.addEventListener("input", () => {
       g.currentColor = color.value;
       g.ctx.fillStyle = color.value;
     });
 
-    const toggleGrid = document.getElementById('toggle-grid');
-    toggleGrid.addEventListener('change', () => {
+    const toggleGrid = document.getElementById("toggle-grid");
+    toggleGrid.addEventListener("change", () => {
       g.grid = toggleGrid.checked;
     });
 
-    const rotate = document.getElementById('randomRotation');
-    rotate.addEventListener('click', () => {
-      for (let i = 0; i < g.shapes.length; i++) {
-        g.shapes[i].angle = Math.random() * Math.PI * 2;
-      }
-    });
-
-    const hSlider = document.getElementById('hslider');
-    hSlider.addEventListener('input', () => {
-      for (let i = 0; i < g.shapes.length; i++) {
-        g.shapes[i].hScale = hSlider.value;
-      }
-    });
-
-    const vSlider = document.getElementById('vslider');
-    vSlider.addEventListener('input', () => {
-      for (let i = 0; i < g.shapes.length; i++) {
-        g.shapes[i].vScale = vSlider.value;
-      }
-    });
-
-    const reset = document.getElementById('reset');
-    reset.addEventListener('click', () => {
-      for (let i = 0; i < g.shapes.length; i++) {
-        g.shapes[i].vScale = 1;
-        g.shapes[i].hScale = 1;
-        g.shapes[i].angle = 0;
-      }
-    });
-
-    const randShape = document.getElementById('randomShape');
-    const shapeTypes = ['rect', 'circle', 'arc-ul', 'arc-ur', 'arc-lr', 'arc-ll', 'tri-ur', 'tri-ul', 'tri-lr', 'tri-ll'];
-    randShape.addEventListener('click', () => {
-      for (let i = 0; i < g.shapes.length; i++) {
-        const shapeIndex = Math.floor(Math.random() * shapeTypes.length);
-        g.shapes[i].type = shapeTypes[shapeIndex];
-      }
-    });
-
-    const exportShape = document.getElementById('export');
-    exportShape.addEventListener('click', () => {
+    const exportShape = document.getElementById("export");
+    exportShape.addEventListener("click", () => {
       const txt = JSON.stringify(g.shapes);
-      const file = new File([txt], 'logo.json', { type: 'text/plain' });
+      const file = new File([txt], `ccbt-${Date.now()}.json`, { type: "text/plain" });
       const url = URL.createObjectURL(file);
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = url;
       link.download = file.name;
       document.body.appendChild(link);
@@ -74,22 +41,32 @@ class Tool {
       window.URL.revokeObjectURL(url);
     });
 
-    const importShape = document.getElementById('import');
-    importShape.addEventListener('change', e => {
-      const file = e.target.files[0];
-      const reader = new FileReader();
-      reader.addEventListener('load', e => {
-        const shapes = JSON.parse(e.target.result);
-        for(let i=0; i<shapes.length; i++) {
-          let s = new Shape(shapes[i].x, shapes[i].y, shapes[i].type, shapes[i].color);
-          g.shapes.push(s);
-        }
-      });
-      reader.readAsText(file);
+    const importShape = document.getElementById("import");
+    importShape.addEventListener("click", (e) => {
+      const input = document.createElement("input");
+      input.type = "file";
+      input.onchange = () => {
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.addEventListener("load", (e) => {
+          const shapes = JSON.parse(e.target.result);
+          for (let i = 0; i < shapes.length; i++) {
+            let s = new Shape(
+              shapes[i].x,
+              shapes[i].y,
+              shapes[i].type,
+              shapes[i].color
+            );
+            g.shapes.push(s);
+          }
+        });
+        reader.readAsText(file);
+      };
+      input.click();
     });
 
-    const clear = document.getElementById('clear');
-    clear.addEventListener('click', () => {
+    const clear = document.getElementById("clear");
+    clear.addEventListener("click", () => {
       g.shapes = [];
       g.ctx.clearRect(0, 0, g.CANVAS_WIDTH, g.CANVAS_HEIGHT);
     });
